fe42dec8a617e6864b5bd502427ec653e96504e2
Zhuo FENG <fenprace.i@gmail.com>
2021-09-19 03:14:38 +1000

    update structure

--- link.sh
+++ link.sh
@@ -1,7 +1,5 @@
 #! /bin/sh
 
-ln -fnsv ${PWD}/i3 ~/.config/i3
-ln -fnsv ${PWD}/networkmanager-dmenu ~/.config/networkmanager-dmenu
 ln -fnsv ${PWD}/polybar ~/.config/polybar
 ln -fnsv ${PWD}/rofi ~/.config/rofi
 ln -fnsv ${PWD}/share ~/.config/share
